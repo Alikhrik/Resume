3b9571e25b50d11643962972ea95f5a0563e0629
Alikhrik <siderdt@gmail.com>
2024-06-27 15:29:45 +0300

    HW 3: Task 2

--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ExperienceComponent } from '../experience/experience.component';
+import { SkillComponent } from '../skill/skill.component';
 
 @Component({
   selector: 'app-person',
   standalone: true,
-  imports: [ExperienceComponent],
+  imports: [ExperienceComponent, SkillComponent],
   templateUrl: './person.component.html',
   styleUrl: './person.component.css'
 })
